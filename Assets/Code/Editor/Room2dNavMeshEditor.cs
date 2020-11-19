440d932709d70b6737893ad174ccdd65b0f4052b
virtouso <s.moeen.kamali@gmail.com>
2020-11-19 15:49:42 +0330

    Updated the editor

---
+++ Assets/Code/Editor/Room2dNavMeshEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class Room2dNavMeshEditor : EditorWindow
+{
+
+    [MenuItem(Room2dNavMeshReferences.NavMeshMenuBarDirectory)]
+    public static void OpenEditorWindow() 
+    {
+        var window = GetWindow<Room2dNavMeshEditor>();
+        window.title = Room2dNavMeshReferences.NavMeshWindowTitlle;
+    }
+
+
+    private bool ShowAStar=false;
+    private bool ShowFloydWarshall = false;
+    private void OnGUI()
+    {
+        GUILayout.Label("Options");
+        if (GUILayout.Button("A*")) { ShowAStar = !ShowAStar; }
+        if (GUILayout.Button("Floyd Warshall")) { ShowFloydWarshall = !ShowFloydWarshall; }
+
+
+        ShowAStar = EditorGUILayout.Foldout(ShowAStar,"A* Settings");
+
+        if (ShowAStar) {
+            GUILayout.Label("A* Setting:"); 
+        
+        }
+        ShowFloydWarshall = EditorGUILayout.Foldout(ShowFloydWarshall, "FloydWarshall Settings");
+
+        if (ShowFloydWarshall) { GUILayout.Label("FloydWarshall Setting:"); }
+    }
+
+}
---
+++ Assets/Code/Static/Room2dNavMeshReferences.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Room2dNavMeshReferences 
+{
+    public const string NavMeshMenuBarDirectory = "Room/NavMesh";
+    public const string NavMeshWindowTitlle = "NavMesh Editor";
+    
+}
